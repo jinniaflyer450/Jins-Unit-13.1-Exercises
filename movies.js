071fc201238f61313a08c03398a1ed41b157ebc3
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-03-27 10:21:15 -0500

    figured out removing movies

--- movies.js
+++ movies.js
@@ -1,9 +1,15 @@
 //Figured out how to use radio buttons here: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+//Figured out how to select the right radio button here https://tutorialdeep.com/knowhow/get-selected-radio-button-value/
 
-
-$('#movie-data-inputs').submit(function(){
+$('#movie-data-inputs').submit(function(e){
+    e.preventDefault();
     let newTitle = $('#movie-title').val();
-    let newRating = $('input[name = rating]').val();
-    console.log(newRating);
-    $('#existing-movies').append(`<p>Movie: ${newTitle} Rating: ${newRating}</p>`);
-})
+    let newRating = $('input[name = rating]:checked').val();
+    $('#existing-movies').append(
+        `<div class = 'movie'>
+        <p><b>Movie:</b> ${newTitle}</p><p><b>Rating:</b> ${newRating}</p>
+        <button class = 'remove-movie'>Remove Movie</button></div>`);
+        $('.movie').on('click', '.remove-movie', function(){
+            $(this).parent().remove();
+        })
+    })
